0bba6e37b4a621b30bda73d84af708c54b41d16b
bwarnick <bwarnick@E6420-8LPNCS1.ecnext03.local>
2015-05-08 20:14:20 -0400

    Final changes today

--- src/com/manta/company/NameWordRunner.java
+++ src/com/manta/company/NameWordRunner.java
@@ -15,10 +15,12 @@ import com.fasterxml.jackson.core.JsonParseException;
 
 public class NameWordRunner {
 
-  private String inputs = "c:/io/matching/xhackathon_mc2_mcp_all00000000000";
-  private File output = new File( "c:/io/mc2_name_words_all.csv" );
+  private String inputs = "c:/io/matching/xhackathon_mc2_mcp_all_";
+  private File output = new File( "c:/io/matching/mc2_name_words_dupes4.csv" );
   private File name_words = new File( "conf/resources/data/name_words.ser" );
   private HashMap<String, String> names = new HashMap<String, String>();
+  private int counter = 0;
+  private int segment = 100000;
 
 
   public NameWordRunner( ) {
@@ -27,50 +29,80 @@ public class NameWordRunner {
 
   public void nameFileParser( ) throws JsonParseException, IOException, ClassNotFoundException {
     console( "Start" );
-    File input = new File( inputs + 0 + ".csv" );
     output.createNewFile();
     NameCleaner cleaner = new NameCleaner();
     @SuppressWarnings( "unused" )
     WordsHash hashmap = new WordsHash( name_words );
+    File input;
     String[] codat;
     String geodna;
     String bline = "";
+    String subline = "";
     String hash = "";
     int i = 0;
+    int j = 0;
 
     FileOutputStream fos = new FileOutputStream( output );
     BufferedWriter bow = new BufferedWriter( new OutputStreamWriter( fos ) );
 
-    FileInputStream fstream = new FileInputStream( input );
-    DataInputStream in = new DataInputStream( fstream );
-    BufferedReader br = new BufferedReader( new InputStreamReader( in ) );
-
     try {
-      // while ( ( bline = br.readLine() ) != null ) {
-      while ( i < 1000000 ) {
+      while ( j < 4 ) {
 
-        bline = br.readLine();
-        codat = bline.split( "," );
-        geodna = GeoDNA.encode( Long.parseLong( codat[2], 10 ), Long.parseLong( codat[3], 10 ) );
-        bline = cleaner.Clean( codat[1] );
-        NameWords words = new NameWords( bline );
-        hash = geodna + words.nameHash();
-        if ( names.containsKey( hash ) ) {
-          console( codat[0] + "-" + names.get( hash ) + "-" + hash );
-          bow.write( codat[0] + "-" + names.get( hash ) + "-" + hash );
-          bow.newLine();
-        } else {
-          names.put( hash, codat[0] );
+        console( "Processing file " + j );
+        input = new File( inputs + j );
+        FileInputStream fstream = new FileInputStream( input );
+        DataInputStream in = new DataInputStream( fstream );
+        BufferedReader br = new BufferedReader( new InputStreamReader( in ) );
+
+        while ( ( bline = br.readLine() ) != null ) {
+          // while ( i < 1000 ) {
+          // bline = br.readLine();
+
+          if ( bline.contains( "latitude" ) || ( bline.contains( "0,0" ) ) ) {
+            bline = br.readLine();
+          }
+          if ( bline.contains( "\"" ) ) {
+            bline = bline.replace( "$", "" );
+            int b = bline.indexOf( "\"" );
+            int e = bline.indexOf( "\"", b + 1 );
+            subline = bline.substring( b + 1, e );
+            subline = subline.replace( ",", " " );
+            subline = subline.replace( "  ", " " );
+            bline = bline.replaceAll( "\".*?\"", subline );
+          }
+          codat = bline.split( "," );
+          codat[2] = codat[2].replaceAll( " ", "." );
+          codat[3] = codat[3].replaceAll( " ", "." );
+          geodna = GeoDNA.encode( Double.parseDouble( codat[2] ), Double.parseDouble( codat[3] ) );
+          bline = cleaner.Clean( codat[1] );
+          NameWords words = new NameWords( bline );
+          hash = geodna + words.nameHash();
+          // console( codat[0] + " - " + hash );
+          if ( names.containsKey( hash ) ) {
+            // console( codat[0] + "-" + names.get( hash ) + "-" + hash );
+            bow.write( codat[0] + "-" + names.get( hash ) + "-" + hash );
+            bow.newLine();
+          } else {
+            names.put( hash, codat[0] );
+          }
+          i++;
+          counter++;
+          if ( counter % segment == 0 ) {
+            console( String.valueOf( counter / segment ) );
+          }
         }
-        i++;
+        j++;
+        br.close();
       }
+      bow.close();
+
     } catch ( Exception e ) {
+      System.out.println( subline );
+      System.out.println( bline );
       e.printStackTrace();
       // TODO Auto-generated catch block
     }
     console( "Done" );
-    br.close();
-    bow.close();
   }
 
 
